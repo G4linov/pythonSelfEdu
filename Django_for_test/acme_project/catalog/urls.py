c1b8f2781262679f14f0bc55b28037d511c46ed2
Vyacheslavvvv <galin0ff@yandex.ru>
2024-11-02 16:47:02 +0300

    New form

--- Django_for_test/acme_project/catalog/urls.py
+++ Django_for_test/acme_project/catalog/urls.py
@@ -3,8 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.product_list),
-    path('1/', views.product_detail),
-    path('2/', views.product_detail),
+
 
 ]
